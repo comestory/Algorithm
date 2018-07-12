29ce333e76a0fd8f25800628c8576339626e1e47
paare <paare@naver.com>
2018-07-13 01:45:26 +0900

    이름 변경

---
+++ Assets/Script/ButtonState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonStatus
+{
+	rightSpin = 0,
+	leftSpin,
+	step,
+	roop,
+	end
+}
+
+public class ButtonState : MonoBehaviour {
+
+	public ButtonStatus state;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
